23a6248ee9b4d68f79637f5d130fc5caa308937f
brendies02 <75529590+brendies02@users.noreply.github.com>
2020-12-06 01:24:58 +1100

    Update Count.lua

--- Count.lua
+++ Count.lua
@@ -2,10 +2,10 @@ local reactor, PowerCurrent, PowerCount
 reactor = peripheral.wrap("back")
 PowerCurrent = reactor.getEnergyProducedLastTick()
 
-function PowerCount()
+function PowerCount ()
     reactor.setActive(false)
     while PowerCurrent > 0 do
-            PowerCount() = PowerCount + 1
+            PowerCount = PowerCount + 1
             sleep(1)
         return PowerCount
     end
