704be2e5b03a41b88948b0552ee134f5156246a1
Adam Bowers <adambowers03@gmail.com>
2020-12-06 01:33:50 +1100

    Update Count.lua
    edits

--- Count.lua
+++ Count.lua
@@ -1,14 +1,14 @@
 local reactor, PowerCurrent, PowerCount
 reactor = peripheral.wrap("back")
-PowerCurrent = reactor.getEnergyProducedLastTick()
 
 function PowerCount ()
+    PowerCount = 0 -- explicit decleration of powercounts starting value. I dont know how lua processes nil + 1
     reactor.setActive(false)
-    while PowerCurrent > 0 do
-            PowerCount() = PowerCount + 1
-            sleep(1)
-        return PowerCount
+    while reactor.getEnergyProducedLastTick() > 0 do
+            PowerCount = PowerCount + 1 -- you dont set a function equal to a value
+            sleep(1)  -- moved return outside of while loop. it'll end the function before it finishes is inside
     end
+    return PowerCount
 end
 sleep(1)   
 print(PowerCount())
